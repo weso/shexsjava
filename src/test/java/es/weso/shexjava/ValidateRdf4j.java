1ea268d090ef8f3cb5c55c56530fa3fc30e3f94d
Jose Labra <jelabra@gmail.com>
2018-06-20 10:34:58 +0200

    Updated RDF4j example

--- src/test/java/es/weso/shexjava/ValidateRdf4j.java
+++ src/test/java/es/weso/shexjava/ValidateRdf4j.java
@@ -1,7 +1,7 @@
 package es.weso.shexjava;
 
+import es.weso.rdf.rdf4j.RDF4jMapper;
 import es.weso.shapeMaps.*;
-import es.weso.shex.ShapeLabel;
 import es.weso.shex.validator.Validator;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
@@ -12,23 +12,21 @@ import org.eclipse.rdf4j.model.vocabulary.FOAF;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.junit.Assert;
 import org.junit.Test;
-
-import es.weso.rdf.PrefixMap;
 import es.weso.rdf.jena.RDFAsJenaModel;
 import es.weso.rdf.rdf4j.RDFAsRDF4jModel;
 import es.weso.shex.Schema;
 import scala.Option;
-import scala.collection.immutable.List;
-import scala.collection.immutable.Map;
 import scala.util.Either;
-import scala.util.Try;
 
 public class ValidateRdf4j {
 	private static final Option<String> none = Option.empty();
 	private static final RDFAsJenaModel emptyRDF = RDFAsJenaModel.apply();
+	private static final Status Conformant = Conformant$.MODULE$ ;
+    private static final Status NonConformant = NonConformant$.MODULE$ ;
+    private static final Status Undefined = Undefined$.MODULE$ ;
 
 
-	@Test
+    @Test
     public void ValidateListStatements() {
         try {
             // The next lines are just taken from: http://docs.rdf4j.org/rdf-tutorial/#_example_01_building_a_simple_model
@@ -46,7 +44,7 @@ public class ValidateRdf4j {
         			+ "prefix : <http://example.org/>"
         			+ "prefix xsd: <http://www.w3.org/2001/XMLSchema#> "
         			+ ":ArtistShape { "
-        			+ "  foaf:name xsd:integer "
+        			+ "  foaf:name xsd:string "
         			+ "}" 
         			;
 
@@ -56,19 +54,21 @@ public class ValidateRdf4j {
         	// Validation
         	Either<String,ResultShapeMap> result = Schema.fromString(strSchema, "ShExC" , none,emptyRDF).flatMap(schema ->
 			ShapeMap.fromString(strShapeMap, "Compact", none, rdf.getPrefixMap(), schema.prefixMap()).flatMap(queryMap ->
-			ShapeMap.fixShapeMap(queryMap,rdf,rdf.getPrefixMap(),schema.prefixMap()).flatMap(shapeMap ->
-			 Validator.validate(schema,shapeMap,rdf)
-			)));
+			ShapeMap.fixShapeMap(queryMap,rdf,rdf.getPrefixMap(),schema.prefixMap()).flatMap(shapeMap -> {
+                        System.out.println("RDF: " + rdf.serialize("TURTLE"));
+                        System.out.println("Schema: " + Schema.serialize(schema,"ShExC",emptyRDF));
+                        System.out.println("ShapeMap" + shapeMap.serialize("Compact") );
+                        return Validator.validate(schema, shapeMap, rdf);
+                    }
+            )));
 
         	// Some checks
             System.out.println("Result: " + result);
         	Assert.assertEquals(result.isRight(),true);
         	ResultShapeMap resultMap = result.right().get();
 
-
-            es.weso.rdf.nodes.IRI artistShapeLabel = es.weso.rdf.nodes.IRI.apply("http://example.org/ArtistShape");
-        	es.weso.shapeMaps.ShapeMapLabel artistLabel = IRILabel.apply(artistShapeLabel);
-        	System.out.println("Conformant shapes: " + resultMap.getConformantShapes(es.weso.rdf.nodes.IRI.apply(picasso.toString())));
+         	ShapeMapLabel artistLabel = IRILabel.apply(RDF4jMapper.iri2iri(vf.createIRI(ex, "ArtistShape")));
+        	Assert.assertEquals(resultMap.getInfo(RDF4jMapper.iri2iri(picasso), artistLabel).status(),Conformant);
 
         } catch (Exception e) {
             e.printStackTrace();
